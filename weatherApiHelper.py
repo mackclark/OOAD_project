725bccf70f8ec408c80f9d1a414e7ad80cf043f9
MacKenzie Clark <mackenzieclark@MacKenzies-MacBook-Pro.local>
2021-04-04 09:46:52 -0400

    removes API key

--- weatherApiHelper.py
+++ weatherApiHelper.py
@@ -5,7 +5,7 @@ import requests
 
 class WeatherApiHelper():
     def __init__(self, location):
-        self.url = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + str(location.lat) + '&lon=' + str(location.long) + '&exclude=minutely&units=imperial&appid=df5f5d432a0b16123a61eea90ab43718'
+        self.url = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + str(location.lat) + '&lon=' + str(location.long) + '&exclude=minutely&units=imperial&appid=[YOUR API KEY HERE]'
         self.response = None
     def getWeatherData(self): 
         resp = requests.get(self.url)
